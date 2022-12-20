e284ecaa3d46b7bf1a9c844513eb4d22a337e40e
dengdongdong <dongvip1617@163.com>
2022-12-20 12:54:57 +0800

    简单接口限流：使用redis、计数器算法实现限流

--- src/main/java/com/xxxx/seckill/vo/RespBeanEnum.java
+++ src/main/java/com/xxxx/seckill/vo/RespBeanEnum.java
@@ -29,8 +29,8 @@ public enum RespBeanEnum {
     REPEATE_ERROR(500501, "该商品每人限购一件"),
     REQUEST_ILLEGAL(500502, "请求非法，请重新尝试"),
     ERROR_CAPTCHA(500503, "验证码错误，请重新输入"),
-
-    ORDER_NOT_EXIST(500300, "订单不存在"),
+    ACCESS_LIMIT_REAHCED(500504, "访问过于频繁，请稍后再试"),
+    ORDER_NOT_EXIST(500300, "订单信息不存在"),
 
     ;
 
